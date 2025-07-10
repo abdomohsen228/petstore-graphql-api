07ff84192919e7ca2f15918073eed11483944bab
abdelrhman mohsen <abdo.mohsen3457643@gmail.com>
2025-07-10 02:13:47 +0200

    finish mutations

--- src/pets/pets.resolver.ts
+++ src/pets/pets.resolver.ts
@@ -1,6 +1,7 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { PetsService } from './pets.service';
 import { Pet } from './pet.entity';
+import { CreatePetInput } from './dto/create-pet-input';
 
 @Resolver(() => Pet)
 export class PetsResolver {
@@ -9,5 +10,11 @@ export class PetsResolver {
   @Query(() => [Pet])
   pets(): Promise<Pet[]> {
     return this.petService.findAll();
+  }
+  @Mutation(() => Pet)
+  createPet(
+    @Args('createPetInput') createPetInput: CreatePetInput,
+  ): Promise<Pet> {
+    return this.petService.createPet(createPetInput);
   }
 }
